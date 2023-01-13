389174f64dc0c25e5f315dcffe2e5fd38503525c
Pal <palmarti@cisco.com>
2023-01-13 14:27:54 +0100

    Testing package

--- Sources/CirclePongLib/CirclePongLib.swift
+++ Sources/CirclePongLib/CirclePongLib.swift
@@ -1,6 +1,27 @@
-public struct CirclePongLib {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+import SwiftUI
+import SpriteKit
+
+@available(iOS 14.0, *)
+public struct CirclePongLib: View {
+    
+    var scene: SKScene {
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+        let scene = GameScene()
+        scene.size = CGSize(width: width, height: height)
+        scene.scaleMode = .fill
+        scene.backgroundColor = .black
+        return scene
     }
+
+   
+    public var body: some View {
+        SpriteView(scene: scene)
+            //.frame(width: 300, height: 400)
+            .ignoresSafeArea()
+    }
+    
+    //public init() {
+    //}
 }
